422f962dda35fc07d9662e8b44dc8126577202e6
tsaboto <tsaboto@gmail.com>
2021-10-02 00:30:17 -0400

    add report

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,21 @@ require "capybara"
 require "capybara/cucumber"
 require "webdrivers"
 
+
+case ENV["BROWSER"]
+when "firefox"
+    @driver = :selenium
+when "chrome"
+    @driver = :selenium_chrome
+when "fire_headless"
+    @drive = :selenium_headless
+when "chrome_headless"
+    @driver = :selenium_chrome_headless
+else
+    raise "Invalid Browser, the variable @dirver is empty "    
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = @driver
     config.default_max_wait_time = 10
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,7 @@
 Before do
     @home_page = HomePage.new
 end
+
+After do
+    page.save_screenshot("logs/screenshot.png")
+end
